5acb0734459c468d6dd04e2c22acde3cf1d5b45e
aboura <bouchrabichou638@gmail.com>
2023-08-12 22:30:50 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const element = document.querySelector('main#main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "bouchra-ikram is the champion"; // Replace 'YOUR-NAME' with the actual name
+
+document.body.appendChild(newHeader);
+
